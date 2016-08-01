a617e8a0c241580ad13d0df655ffbc35f098bce4
Kevin Ma <kbma.kevin@gmail.com>
2016-08-01 19:57:38 -0400

    version 0.1.4 - updated config.scene.ts to include other scenes

--- Scripts/config/scene.js
+++ Scripts/config/scene.js
@@ -4,21 +4,37 @@
  * @studentID 300867968
  * @date August 1, 2016
  * @description This file stores game constants for the different scenes
- * @version 0.1.0 - initial commit
+ * @version 0.1.4 - updated config.scene.ts to include other scenes
  */
 var config;
 (function (config) {
     var Scene = (function () {
         function Scene() {
         }
+        ;
         Scene.MENU = 0;
+        Scene.OVER = 20;
         // Level 1 Constants
-        Scene.STORY1 = 0;
-        Scene.INSTRUCTIONS1 = 0;
-        Scene.STAGELOADING1 = 0;
-        Scene.LEVEL1 = 0;
-        Scene.CONTINUE1 = 0;
-        Scene.SHOP1 = 0;
+        Scene.STORY1 = 1;
+        Scene.INSTRUCTIONS1 = 2;
+        Scene.STAGELOADING1 = 3;
+        Scene.LEVEL1 = 4;
+        Scene.SHOP1 = 5;
+        Scene.CONTINUE1 = 6;
+        // Level 2 Constants
+        Scene.STORY2 = 7;
+        Scene.INSTRUCTIONS2 = 8;
+        Scene.STAGELOADING2 = 9;
+        Scene.LEVEL2 = 10;
+        Scene.SHOP2 = 11;
+        Scene.CONTINUE2 = 12;
+        // Level 3 Constants
+        Scene.STORY3 = 13;
+        Scene.INSTRUCTIONS3 = 14;
+        Scene.STAGELOADING3 = 15;
+        Scene.BOSS1 = 16;
+        Scene.BOSS2 = 17;
+        Scene.BOSS3 = 18;
         return Scene;
     }());
     config.Scene = Scene;
--- Scripts/config/scene.ts
+++ Scripts/config/scene.ts
@@ -4,21 +4,37 @@
  * @studentID 300867968
  * @date August 1, 2016
  * @description This file stores game constants for the different scenes
- * @version 0.1.0 - initial commit
+ * @version 0.1.4 - updated config.scene.ts to include other scenes
  */
 module config {
     export class Scene {
+
         public static MENU: number = 0;
+        public static OVER: number = 20;
 
         // Level 1 Constants
-        public static STORY1: number = 0;
-        public static INSTRUCTIONS1: number = 0;
-        public static STAGELOADING1: number = 0;
-        public static LEVEL1: number = 0;
-        public static CONTINUE1: number = 0;
-        public static SHOP1: number = 0;
+        public static STORY1: number = 1;
+        public static INSTRUCTIONS1: number = 2;
+        public static STAGELOADING1: number = 3;
+        public static LEVEL1: number = 4;;
+        public static SHOP1: number = 5;
+        public static CONTINUE1: number = 6;
 
-        
+        // Level 2 Constants
+        public static STORY2: number = 7;
+        public static INSTRUCTIONS2: number = 8;
+        public static STAGELOADING2: number = 9;
+        public static LEVEL2: number = 10;
+        public static SHOP2: number = 11;
+        public static CONTINUE2: number = 12;
+
+        // Level 3 Constants
+        public static STORY3: number = 13;
+        public static INSTRUCTIONS3: number = 14;
+        public static STAGELOADING3: number = 15;
+        public static BOSS1: number = 16;
+        public static BOSS2: number = 17;
+        public static BOSS3: number = 18;
 
     }
 }
