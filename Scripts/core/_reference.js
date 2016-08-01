43c3f524b0e40634693cfc4c6b656a6fb73f4a16
Kevin Ma <kbma.kevin@gmail.com>
2016-08-01 17:00:47 -0400

    removed unnecessary files for this project

--- Scripts/core/_reference.js
+++ Scripts/core/_reference.js
@@ -5,7 +5,5 @@
 /// <reference path="../objects/label.ts"/>
 /// <reference path="../objects/scene.ts"/>
 /// <reference path="../objects/ocean.ts"/>
-/// <reference path="../scenes/over.ts"/>
-/// <reference path="../scenes/play.ts"/>
 /// <reference path="../scenes/menu.ts"/> 
 //# sourceMappingURL=_reference.js.map
